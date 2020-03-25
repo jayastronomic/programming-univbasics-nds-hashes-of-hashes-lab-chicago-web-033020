f03be648d91a8bc2278224359ebb367f7235a935
Julian Antonio Smith <juliansmith94@hotmail.com>
2020-03-25 16:29:55 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -43,13 +43,22 @@ def naming_system
   # HoH all the way to Species!
   
   biological_life_hash = { 
-    label: Kingdom,
+    label: "Kingdom",
     sub_category: {
-     label: Phylum,
+     label: "Phylum",
      sub_category: {
-       label: Class,
+       label: "Class",
        sub_category: {
-         label: 
+         label: "Order",
+         sub_category: {
+           label: "Family",
+           sub_category: { 
+             label: "Genus",
+             sub_category: {
+               label:
+             }
+           
+         }
        }
      }
    }
